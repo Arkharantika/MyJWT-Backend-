d12a1db45933c6ae3834cbc1a1f47a126be0a073
Arkharantika <paradies7@student.uns.ac.id>
2023-05-03 10:49:19 +0700

    first commit

---
+++ config/Database.js
@@ -0,0 +1,17 @@
+// import { Sequelize } from "sequelize";
+
+// const db = new Sequelize('auth_db','root','',{
+//     host: "localhost",
+//     dialect: "mysql"
+// })
+
+// export default db;
+
+import {Sequelize} from "sequelize";
+
+const db = new Sequelize('auth_db','root','',{
+    host: "localhost",
+    dialect: "mysql"
+});
+
+export default db;
